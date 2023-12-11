6d34ee7f263a7b1a0b8e1f9cc8e2ccb3c6b9f7d8
Gustavo Gonzalez <gagm0995@gmail.com>
2023-12-11 11:02:00 -0300

    app de peliculas con js

---
+++ script.js
@@ -0,0 +1,50 @@
+document.getElementById('searchButton').addEventListener('click', searchMovies)
+let api_key = 'f5a0796d3f66581e64e73abfecbdc218'
+let urlBase =  'https://api.themoviedb.org/3/search/movie'
+let urlImg = 'https://image.tmdb.org/t/p/w200'
+
+
+function searchMovies(){
+    let searchInput = document.getElementById('searchInput').value
+
+    fetch(`${urlBase}?api_key=${api_key}&query=${searchInput}`)
+    .then(response => response.json())
+    .then(response => displayMovies(response.results))
+}
+
+function displayMovies(movies){
+    let resultContainer = documet.getElementById('results')
+    resultContainer.innerHTML = ''
+
+    if(movies.length ===0 ){
+        resultContainer.innerHTML = '<p>No se encontro resultado para tu busqueda</p>'
+        return
+    }
+
+    movies.foreach(movie => {
+        let movieDiv = document.createElement('div')
+        movieDiv.classList.add('movie')
+
+        let tittle = document.createElement('h2')
+        tittle.textContent = movie.tittle
+
+        let releaseData = document.createElement('p')
+        releaseData.textContent = 'La fecha de lanzamiento fue: ' + movie.release_date
+
+        let overview = document.createElement('p')
+        overview.textContent = movie.overview
+
+        let posterPath = urlImg + movie.poster_path
+        let poster = document.createElement(img)
+        poster.src = posterPath
+
+        movieDiv.appendChild(poster)
+        movieDiv.appendChild(tittle)
+        movieDiv.appendChild(releaseData)
+        movieDiv.appendChild(overview)
+
+        resultContainer.appendChild(movieDiv)
+
+
+    })
+}
